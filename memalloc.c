9bbe76400c2cd42b918118b0c1fcdf48d4a5c8b0
Sahidev1 <theoddrogue@gmail.com>
2023-07-08 14:08:56 +0200

    removed printf statements and updated header file

--- memalloc.c
+++ memalloc.c
@@ -1,6 +1,5 @@
 #include "memalloc.h"
 #include <sys/mman.h>
-#include <stdio.h> // delete this, only for debugging.
 
 #define ALLOC_OVERHEAD (sizeof(mem_block))
 
@@ -29,7 +28,6 @@ mem_block* usable_to_base_addr (void* usable_addr){
 
 void* memalloc (size_t block_size){ 
     if (first_block == NULL){
-        printf("first is null\n");
         first_block = alloc_new_block(block_size);
         return usable_alloc_addr (first_block);
     }
@@ -37,30 +35,25 @@ void* memalloc (size_t block_size){
     mem_block* curr_block = first_block;
     mem_block* last_prev_block = curr_block;
     while (curr_block != NULL){
-        printf("iterating :)\n");
-        printf("curr: %p\n", curr_block);
         if (curr_block->is_free && curr_block->size >= block_size){
             curr_block->is_free = FALSE;
-            printf("free block found: %p\n", curr_block);
             return usable_alloc_addr(curr_block);
         }
         if (curr_block->next_block == NULL) last_prev_block = curr_block;
         curr_block = curr_block->next_block;
     }
-    printf("outside of while\n");
     curr_block = alloc_new_block(block_size);
     last_prev_block->next_block = curr_block;
-    printf ("%p\n", curr_block); //for debug
     return usable_alloc_addr(curr_block);
 }
 
+// this only exists for testing purposes!
 mem_block* get_mem_list (){
     return first_block;
 }
 
 int memfree (void* usable_memaddr){
     mem_block* block = usable_to_base_addr(usable_memaddr);
-    printf("freed: %p\n", block);
     block->is_free = TRUE;
     return 0;
 }
--- memalloc.h
+++ memalloc.h
@@ -12,9 +12,7 @@ typedef struct Mem_block {
     struct Mem_block* next_block;
 } mem_block;
 
-mem_block* alloc_new_block (size_t block_size);
 void* memalloc (size_t block_size);
 int memfree (void* usable_memaddr);
-mem_block* get_mem_list();
 
 #endif
